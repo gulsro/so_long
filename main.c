d47ff31b790456e641c1cf605e4fdd2ce06b888e
gozturk <glserozkan@gmail.com>
2023-04-13 17:11:21 +0200

    nice little pink cloud in the middle

--- main.c
+++ main.c
@@ -11,14 +11,14 @@ static void	ft_error(void)
 	perror("error");
 	exit(1);
 }
-
+/*
 static void ft_hook(void* param)
 {
 	const mlx_t* mlx = param;
 
 	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
 }
-
+*/
 int main()
 {
 	mlx_t	*mlx;
@@ -34,16 +34,28 @@ int main()
 		ft_error();
 
 	//drawing the image
-//	mlx_put_pixel(img, 0, 0, )
-//
-	//filling all pixels in image
-	memset(img->pixels, 255, img->width * img->height * BPP);
+	uint32_t i = 0;
+	uint32_t j = 0;
+	while (i < 64)
+	{
+		j = 0;
+		while (j < 64)
+		{
+			mlx_put_pixel(img, i, j, 0xF70AF0B3);
+	//		mlx_put_pixel(img, i, 50, 0xFFFFFFFF);
+			j++;
+		}
+		i++;
+	}
 
-	if (mlx_image_to_window(mlx, img, 110, 10) < 0)
+	//filling all pixels in image
+	//memset(img->pixels, 204, img->width * img->height * BPP);
+
+	if (mlx_image_to_window(mlx, img, (mlx->width)/2-(img->width)/2, (mlx->height)/2-(img->height)/2) < 0)
 		ft_error();
 	
 	//window ll be opened and width and height will be printed on the command line
-	mlx_loop_hook(mlx, ft_hook, mlx);
+//	mlx_loop_hook(mlx, ft_hook, mlx);
 	//now we see the window
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
