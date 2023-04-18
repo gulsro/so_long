607ce9c74a2dff03154c0a75e016878cf870bbe2
gozturk <glserozkan@gmail.com>
2023-04-18 11:40:11 +0200

    break

---
+++ map_utils.c
@@ -0,0 +1,33 @@
+#include "so_long.h"
+
+void    map_scale(t_map *my_map)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (my_map->map_arr[i] != '\0')
+	{
+		i++;
+	}
+	my_map->height_y = i;
+	while (my_map->map_arr[0][j] != '\0')
+	{
+		j++;
+	}
+	my_map->width_x = j;
+}
+
+char	**create_map_arr_cpy(t_map *my_map)
+{
+	int		i;
+	int 	j;
+	char	**cpy_arr;
+	
+	i = 0;
+	cpy_arr = ft_calloc(sizeof(char), my_map->height_y * my_map->width_x + 1);
+	if (!cpy_arr)
+		ft_exit("Map array cant be copied", 1);
+	while ()
+}
